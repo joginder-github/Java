e51107cb297831f549172a01d55277e78de4d25a
Joginder Pal <lko.jetking.joginder@gmail.com>
2019-06-13 03:54:31 +0530

    Static Class
    Demonstration of static class.

---
+++ Hello.java
@@ -0,0 +1,14 @@
+class Example
+{
+ static class Test
+ {
+  public static String country="INDIA";
+ }
+}
+public class Hello
+{
+public static void main(String[] args)
+{
+ System.out.println(Example.Test.country);
+}
+}

79bb3e65b3106c1151207982099e151a90a4d5d9
Joginder Pal <lko.jetking.joginder@gmail.com>
2019-06-16 15:09:46 +0530

    initialization block

---
+++ initialization_block_in_java/Test.java
@@ -0,0 +1,17 @@
+public class Test
+{
+ private int x;
+ {
+  System.out.println("Initialization Block:x="+x);
+  x=5;
+ }
+ public Test()
+ {
+  System.out.println("Constructor:x="+x);
+ }
+ public static void main(String[] args)
+ {
+  Test t1=new Test();
+  Test t2=new Test();
+ } 
+}

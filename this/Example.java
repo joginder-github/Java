b0013f41328ed266ec13a8fd2373f73e95377498
Joginder Pal <lko.jetking.joginder@gmail.com>
2019-06-16 15:09:21 +0530

    this keyword.

---
+++ this/Example.java
@@ -0,0 +1,21 @@
+class Box
+{
+ private int l,b,h;
+ public void setDimension(int l,int b,int h)  //instance member function
+ {
+  this.l=l;this.b=b;this.h=h;
+ }
+ public void sendBox()
+ {
+  GiftTaker gf=new GiftTaker();
+  gf.acceptGift(this);
+ }
+}
+public class Example
+{
+ public static void main(String[] args)
+ {
+  Box b1=new Box();
+  b1.setDimension(12,10,15);
+ }
+}

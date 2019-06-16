525213d4956c095386830d7831c28e17dac119e1
Joginder Pal <lko.jetking.joginder@gmail.com>
2019-06-16 15:05:17 +0530

    Inheritance
    student class is inheriting the property of person class and example class is creating object and calling methods of the other class.

---
+++ inheritance/Example.java
@@ -0,0 +1,13 @@
+public class Example
+{
+ public static void main(String[] args)
+ {
+  Student s1=new Student();
+  s1.setRollno(100);
+  s1.setName("Joginder Pal");
+  s1.setAge(18);
+  System.out.println("Roolno:"+s1.getRollno());
+  System.out.println("Age:"+s1.getAge());
+  System.out.println("Name:"+s1.getName());
+ }
+}

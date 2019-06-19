c042be18328e2f061208e457fc2a7fbb06d595ab
Joginder Pal <lko.jetking.joginder@gmail.com>
2019-06-19 22:09:43 +0530

    wrapper class

---
+++ wrapper_class/Wrapper.java
@@ -0,0 +1,16 @@
+public class Wrapper
+{
+ public static void main(String[] args)
+ {
+  Integer i1=Integer.valueOf("101011",2);
+  Double d1=Double.valueOf("3.14");
+  int a=Integer.parseInt("123");
+  double b= Double.parseDouble("13.45");
+  int c= i1.intValue();
+  System.out.println(i1);
+  System.out.println(d1);
+  System.out.println(a);
+  System.out.println(b);
+  System.out.println(i1);
+ }
+}
